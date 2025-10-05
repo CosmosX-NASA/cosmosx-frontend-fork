3f6e47420ff35674e3ca65f83a4978ad06deb397
jaewonE <importjaewonE@gmail.com>
2025-10-05 20:10:32 +0900

    [Config] Add base to defineConfig

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: '/https://github.com/CosmosX-NASA/cosmosx-frontend/',
   server: {
     proxy: {
       '/api': {
