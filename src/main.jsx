0ad888d3aba33b695115af6b278b0551a414d06c
jaewonE <importjaewonE@gmail.com>
2025-10-05 20:23:18 +0900

    [Build] Add gh-pages-3

--- src/main.jsx
+++ src/main.jsx
@@ -1,25 +1,24 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
-import Background from "./components/Background";
+import Background from './components/Background';
 
-import Member from "./pages/member";
-import LandingPage from "./pages/LandingPage";
-import Home from "./pages/home";
-import Paper from "./pages/paper";
-import ResearchGap from "./pages/researchGap";
-import Hypothesis from "./pages/hypothesis";
+import Member from './pages/member';
+import LandingPage from './pages/LandingPage';
+import Home from './pages/home';
+import Paper from './pages/paper';
+import ResearchGap from './pages/researchGap';
+import Hypothesis from './pages/hypothesis';
 
-const root = document.getElementById("root");
+const root = document.getElementById('root');
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(root).render(
   <QueryClientProvider client={queryClient}>
     <Background />
     <div className="relative z-10">
-      {" "}
       <BrowserRouter>
         <Routes>
           <Route index element={<LandingPage />} />
